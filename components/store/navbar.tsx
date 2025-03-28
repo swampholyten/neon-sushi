31ca96eb975ed37358fc1e206de70156d4428741
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2025-03-28 10:37:59 +0100

    add link to home

--- components/store/navbar.tsx
+++ components/store/navbar.tsx
@@ -7,6 +7,7 @@ import { getCategories, getProducts } from "@/lib/db/queries";
 import { Suspense } from "react";
 import { MenuToggle } from "@/components/menu/menu";
 import SignOutButton from "@/components/auth/sign-out-button";
+import Link from "next/link";
 
 const Navbar = () => {
   const productsPromise = getProducts();
@@ -15,14 +16,21 @@ const Navbar = () => {
   return (
     <header className="sticky top-0 z-10 border-b bg-background/95">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
-        <div className="flex items-center gap-4">
-          <div className="size-8 relative">
-            <Image src="/logo.svg" alt="Logo" fill className="object-contain" />
+        <Link href={"/"}>
+          <div className="flex items-center gap-4">
+            <div className="size-8 relative">
+              <Image
+                src="/logo.svg"
+                alt="Logo"
+                fill
+                className="object-contain"
+              />
+            </div>
+            <h1 className="text-xl tracking-tighter sm:text-2xl hidden md:block">
+              Neon Sushi
+            </h1>
           </div>
-          <h1 className="text-xl tracking-tighter sm:text-2xl hidden md:block">
-            Neon Sushi
-          </h1>
-        </div>
+        </Link>
 
         <div className="flex items-center gap-4">
           <Suspense fallback={<Skeleton />}>
